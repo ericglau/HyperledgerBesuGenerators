90726073c337cb1c26fd6b4e22973d3010a1d2e9
Eric Lau <29640130+ericglau@users.noreply.github.com>
2019-11-09 20:30:55 -0500

    Connect to besu

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # BesuGenerator
+
+Build sample project: `mvn clean install`
+
+Build archetype: `mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes -DarchetypeArtifactId=maven-archetype-archetype -DarchetypeVersion=1.4`
+
+Run Besu:
+target/besu-1.3.4/bin/besu --network=dev --rpc-http-enabled=true
+
+Run class:
+mvn exec:java -Dexec.mainClass="com.sample.App"
--- src/main/java/com/sample/App.java
+++ src/main/java/com/sample/App.java
@@ -1,8 +1,18 @@
 package com.sample;
 
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
 public class App {
-	public String Sample() {
-		return "sample";
+	public static void main(String[] args) throws Exception {
+                System.out.println( "Starting web3server" );
+
+                Web3j web3j = Web3j.build(new HttpService("http://localhost:8545"));  // defaults to http://localhost:8545/
+
+                Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+                String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+                System.out.println("Connected to Hyperledger Besu: " + clientVersion);
 	}
 
 }
